32a2ede166ad52da072c1ce4ba6eb2c9630e9e44
Yanna Lin <linyanna@yahoo.com>
2023-01-30 23:04:16 -0500

    fetched post data w axios, dynamic link to post page

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,24 +1,28 @@
-import "./post.css"
+import "./post.css";
+import {Link} from "react-router-dom";
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
-      <img 
+      {post.photo && (
+        <img 
         className="postImg"
-        src="https://i.pinimg.com/originals/3b/1c/24/3b1c24c904e21b92975a7b67d356e94c.jpg" 
+        src={post.photo} 
         alt=""/>
+      )}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map(c => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span 
-          className="postTitle">Lorem ipsum dolor sit amet
-        </span>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
         <hr/>
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
       </div>
-      <p className="postDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus consectetur sint nisi voluptatum rem id earum itaque vitae, culpa enim dolorum aliquam nesciunt placeat consequatur, laborum maiores veniam doloremque quia?</p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.css"
+import axios from "axios" // JS library for making HTTP requests, could also use fetch() function
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts =  async() => {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    }
+    fetchPosts();
+  }, [])
   return (
     <>
       <Header/>
       <div className="home">
-        <Posts />
+        <Posts posts={posts}/>
         <Sidebar />
       </div>
     </>
